9dcdc356e92a1d72cab89b3e9602e7c12dbc2ffd
Anshu00977 <anshuls600@gmail.com>
2025-08-07 15:49:36 +0530

    Moved package.json to project root

--- api/auth.js
+++ api/auth.js
@@ -1,9 +1,6 @@
-// File: api/auth.js
-
 module.exports = (req, res) => {
   const code = req.query.code;
   const state = req.query.state;
-
   if (code) {
     res.send(`✅ TikTok Auth Code: ${code}<br>🔐 State: ${state}`);
   } else {
